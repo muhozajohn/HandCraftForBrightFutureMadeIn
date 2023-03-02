09c5565661e3a0150bdb0bb1e850df1798c06a25
IshKevin <carterk279@gmail.com>
2023-03-02 08:50:41 +0200

    add top nav

--- src/components/NavBar/TopNav.js
+++ src/components/NavBar/TopNav.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import './TopNav.css'
 
 const TopNav = () => {
   return (
-    <div>Top Navigation bar</div>
+    <section>
+      <div className='topnav-sect1' >
+        <p>
+          welcome to made in rwanda handcrafted website
+        </p>
+        <div className='topnav-sect1-left'>
+          <div >avatar</div>
+          <div><a href="#">login</a>/<a href="#">signup</a></div>
+        </div>
+      </div>
+    </section>
   )
 }
 
