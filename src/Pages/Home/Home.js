92baea280f397419405b7baf257091eab4ffe6ba
IshKevin <carterk279@gmail.com>
2023-03-02 10:22:00 +0200

    re structure project

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Body from '../../components/Body/Body'
+import TopNav from '../../components/NavBar/TopNav'
+
+const Home = () => {
+  return (
+    <section>
+        <TopNav className='home-topnav'/>
+        <Body/>
+    </section>
+  )
+}
+
+export default Home
---
+++ src/components/SemNavBar/SemNavBar.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SemNavBar = () => {
+  return (
+    <div>SemNavBar</div>
+  )
+}
+
+export default SemNavBar
