4521505c3736420c0bf1658655fbe6d275cca0e3
IshKevin <carterk279@gmail.com>
2023-03-02 10:32:50 +0200

    add logo

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import Body from '../../components/Body/Body'
 import TopNav from '../../components/NavBar/TopNav'
+import SemNavBar from '../../components/SemNavBar/SemNavBar'
 
 const Home = () => {
   return (
     <section>
         <TopNav className='home-topnav'/>
+        <SemNavBar/>
         <Body/>
     </section>
   )
