675a44703683d17073ce0c4094743c360569d87f
IshKevin <carterk279@gmail.com>
2023-03-02 09:42:10 +0200

    add icon

--- src/components/NavBar/TopNav.js
+++ src/components/NavBar/TopNav.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './TopNav.css'
+import {RxAvatar} from 'react-icons/rx'
 
 const TopNav = () => {
   return (
-    <section>
+    <section className='TopNav'>
       <div className='topnav-sect1' >
         <p>
           welcome to made in rwanda handcrafted website
         </p>
         <div className='topnav-sect1-left'>
-          <div >avatar</div>
+          {/* <div >
+          <div className="Topnav-avatar">
+             <img className="Topnav-avatar__image" src="..." />
+           </div>
+          </div> */}
+          <RxAvatar className='TopNav-icon'/>
           <div><a href="#">login</a>/<a href="#">signup</a></div>
         </div>
       </div>
